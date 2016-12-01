a5ba6bd2539ba02ffe0b17dc896868d6a68a2bc8
Abdullah A. Alam <abdullah.alam.omi@gmail.com>
2016-11-30 23:48:13 -0800

    Added First Programming for Python

---
+++ 01-First-Programming/first-programming.py
@@ -0,0 +1,16 @@
+# This is a Python comment. Lines that begin with a '#' are ignored by the
+# Python interpreter. Comments are useful for documenting code or explaining
+# quiz questions!
+# 
+# Write a Python program that prints out the number of minutes in seven weeks.
+# Remember: 7 weeks 7 days in a week, 24 hours in a day, and 60 mins in an hour.
+# Multiplying these numbers together will give you the result
+#
+# Click the "Test Run" button below to try running your code and see the output,
+# and click "Submit" to submit your answer.
+
+number_of_weeks = 7
+number_of_days = number_of_weeks * 7
+number_of_hours = number_of_days * 24
+number_of_minutes = number_of_hours * 60
+print number_of_minutes
