053f92fe5fe9499e790d2780c63864883438ef8e
Abdullah A. Alam <abdullah.alam.omi@gmail.com>
2016-12-02 01:15:55 -0800

    Added Spirit Age

---
+++ 04-Spirit-Age/spirit-age.py
@@ -0,0 +1,10 @@
+# Write python code that defines the variable 
+# age to be your age in years, and then prints 
+# out the number of days you have been alive.
+
+year = 2016
+age = year - 1987
+day = 365
+number_of_days = age * day
+
+print number_of_days
