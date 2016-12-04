896a3de0599405d0742fce497ef237058dd4fa44
Abdullah A. Alam <abdullah.alam.omi@gmail.com>
2016-12-03 21:06:12 -0800

    Added Capital Udacity

---
+++ 06-Capital-Udacity/capital-udacity.py
@@ -0,0 +1,7 @@
+# Write Python code that prints out Udacity (with a capital U), 
+# given the definition of s below.
+
+s = 'audacity'
+
+t = s[1:]               # udacity
+print t.capitalize()    # Udacity
