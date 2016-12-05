31530fed6e6107d7d19438499613f9c0da6394f3
Abdullah A. Alam <abdullah.alam.omi@gmail.com>
2016-12-05 14:29:05 -0800

    Added Bodacious Udacity

---
+++ 11-Bodacious-Udacity/bodacious-udacity.py
@@ -0,0 +1,16 @@
+# Given the variables s and t defined as:
+s = 'udacity'
+t = 'bodacious'
+# write Python code that prints out udacious
+# without using any quote characters in
+# your code.
+
+
+
+
+x = s[0]    # u
+y = t[2:]   # dacious
+print x + y # udacious
+
+
+
