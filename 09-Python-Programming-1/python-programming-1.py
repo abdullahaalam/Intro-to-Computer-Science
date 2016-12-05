edb62d08d5eb359172af9956758a3204fba56bb3
Abdullah A. Alam <abdullah.alam.omi@gmail.com>
2016-12-05 12:37:52 -0800

    Added Python Programming 1

---
+++ 09-Python-Programming-1/python-programming-1.py
@@ -0,0 +1,12 @@
+# Write Python code that prints out the number of hours in 7 weeks.
+
+
+weeks = 7
+days = 7
+number_of_days = weeks * days
+hours = 24
+number_of_hours = number_of_days * hours
+
+print number_of_hours
+
+
