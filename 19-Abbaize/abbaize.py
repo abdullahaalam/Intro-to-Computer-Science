1e4a45f60375dac6d6faca997137a93a966ea789
Abdullah A. Alam <abdullah.alam.omi@gmail.com>
2016-12-06 15:26:55 -0800

    Added Abbaize

---
+++ 19-Abbaize/abbaize.py
@@ -0,0 +1,23 @@
+# Define a procedure, abbaize, that takes
+# two strings as its inputs, and returns
+# a string that is the first input,
+# followed by two repetitions of the second input,
+# followed by the first input.
+
+
+
+
+
+
+
+
+
+#print abbaize('a','b')
+#>>> 'abba'
+
+#print abbaize('dog','cat')
+#>>> 'dogcatcatdog'
+
+def abbaize(a, b):
+    total = a + b + b + a
+    return total
