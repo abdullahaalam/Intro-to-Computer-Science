6d75b682b6ade1f3fbe66136d760eccbb9174706
Abdullah A. Alam <abdullah.alam.omi@gmail.com>
2016-12-06 15:14:19 -0800

    Added Sum of Three

---
+++ 18-Sum-of-Three/sum-of-three.py
@@ -0,0 +1,23 @@
+# Define a procedure, sum3, that takes three
+# inputs, and returns the sum of the three
+# input numbers.
+# To help you out, the code for sum(a,b) is below.
+
+def sum(a,b):
+    return a + b
+
+
+
+
+
+
+
+#print sum3(1,2,3)
+#>>> 6
+
+#print sum3(93,53,70)
+#>>> 216
+
+def sum3(a, b, c):
+    total = a + b + c
+    return total
