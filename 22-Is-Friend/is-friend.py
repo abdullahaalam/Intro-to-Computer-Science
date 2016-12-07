83b72b03b9ff005ee6622285df30b9b1a07be2c1
Abdullah A. Alam <abdullah.alam.omi@gmail.com>
2016-12-07 14:07:26 -0800

    Added Is Friend

---
+++ 22-Is-Friend/is-friend.py
@@ -0,0 +1,27 @@
+# Define a procedure, is_friend, that
+# takes a string as its input, and
+# returns a Boolean indicating if
+# the input string is the name of
+# a friend. Assume I am friends with
+# everyone whose name starts with D
+# and no one else. You do not need to
+# check for the lower case 'd'
+
+
+
+
+
+
+
+
+#print is_friend('Diane')
+#>>> True
+
+#print is_friend('Fred')
+#>>> False
+
+def is_friend(name):
+    if name.startswith('D'):
+        return True
+    else:
+        return False
