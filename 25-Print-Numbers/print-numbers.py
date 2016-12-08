edd23506cb4ba74652894a2fec3c017a5122893d
Abdullah A. Alam <abdullah.alam.omi@gmail.com>
2016-12-07 21:41:39 -0800

    Added Print Numbers

---
+++ 25-Print-Numbers/print-numbers.py
@@ -0,0 +1,25 @@
+# Define a procedure, print_numbers, that takes
+# as input a positive whole number, and prints 
+# out all the whole numbers from 1 to the input
+# number.
+
+# Make sure your procedure prints "upwards", so
+# from 1 up to the input number.
+
+                  
+   
+
+
+
+#print_numbers(3)
+#>>> 1
+#>>> 2
+#>>> 3
+
+def print_numbers(n):
+    i = 0
+    while i < n:
+        i = i + 1
+        print i
+        
+        
