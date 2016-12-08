45e71c0e185a38b8c034c1583852e61e19a4f6d3
Abdullah A. Alam <abdullah.alam.omi@gmail.com>
2016-12-08 13:41:38 -0800

    Added Udacify

---
+++ 28-Udacify/udacify.py
@@ -0,0 +1,31 @@
+# Define a procedure, udacify, that takes as
+# input a string, and returns a string that
+# is an uppercase 'U' followed by the input string.
+# for example, when you enter
+
+# print udacify('dacians')
+
+# the output should be the string 'Udacians'
+
+
+
+
+
+
+
+# Remove the hash, #, from infront of print to test your code.
+
+#print udacify('dacians')
+#>>> Udacians
+
+#print udacify('turn')
+#>>> Uturn
+
+#print udacify('boat')
+#>>> Uboat
+
+
+
+
+def udacify(name):
+    return 'U' + name
